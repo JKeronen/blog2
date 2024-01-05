afd9ca98fb1414630fe1769559722ea125c243ab
Jan Keronen <keronen.jan@gmail.com>
2024-01-05 09:47:37 +0200

    Done to exercise 4.19

--- controllers/login.js
+++ controllers/login.js
@@ -1,2 +1,32 @@
-const blogRouter = require('express').Router()
-const User = require('../models/kauttaja')
+const loginRouter = require('express').Router()
+const User = require('../models/kayttaja')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+loginRouter.post('/', async (request, response) => {
+    const { username, password } = request.body
+  
+    const user = await User.findOne({ username })
+    const passwordCorrect = user === null
+      ? false
+      : await bcrypt.compare(password, user.passwordHash)
+    console.log(passwordCorrect)
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: 'invalid username or password'
+      })
+    }
+  
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    }
+  
+    const token = jwt.sign(userForToken, process.env.SECRET)
+  
+    response
+      .status(200)
+      .send({ token, username: user.username, name: user.name })
+  })
+  
+  module.exports = loginRouter
