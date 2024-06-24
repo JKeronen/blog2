66fbe182fe2c6c364cf8b82204f0f371e085a5d4
Jan Keronen <keronen.jan@gmail.com>
2024-06-24 14:41:36 +0300

    Address bug fixed

--- controllers/testing.js
+++ controllers/testing.js
@@ -1,6 +1,6 @@
 const testRouter = require('express').Router()
 const Blog = require('../models/postaus')
-const User = require('../models/kauttaja')
+const User = require('../models/kayttaja')
 
 testRouter.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
